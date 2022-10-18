33649fb8baa6e97c50dc3602ccb4cceb3f909662
ShayMeshGit <shaymesh420@gmail.com>
2022-10-18 13:38:00 +0300

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import type { AppPropsWithLayout } from "./types";
+import type { AppPropsWithLayout } from "./index";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import type { NextPageWithLayout } from "./types";
-import type { ReactElement } from "react";
 import HomeLayout from "../Layouts/HomeLayout";
 import LandingSection from "../components/LandingSection";
 import WatchOnTvSection from "../components/WatchOnTvSection";
@@ -7,9 +5,25 @@ import DownloadAndWatchSection from "../components/DownloadAndWatchSection";
 import WatchOnDeviceSection from "../components/WatchOnDeviceSection";
 import KidsValueSection from "../components/KidsValueSection";
 import FAQSection from "../components/FAQSection";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
 const Home: NextPageWithLayout = () => {
   return (
     <>
+      <Head>
+        <title>Netflix - Shay Mesh</title>
+      </Head>
       <LandingSection />
       <WatchOnTvSection />
       <DownloadAndWatchSection />
